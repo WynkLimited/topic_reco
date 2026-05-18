908b7e23a53e6979b3950e25a8675121d32937b1
ritika-wynk <ritika.yadav@airtel.com>
2026-05-18 11:08:31 +0000

    added reco files

---
+++ als_topic_reco.py
@@ -0,0 +1,256 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.utils import AnalysisException
+from pyspark.sql import functions as F
+from pyspark.sql.window import Window
+from datetime import date, datetime, timedelta
+
+from pymongo import MongoClient
+from urllib.parse import quote_plus
+import numpy as np
+import gcsfs
+from pyspark.ml.recommendation import ALS
+
+def get_spark():
+    return (
+        SparkSession.builder
+        .appName("ALS Topic Reco")
+        .getOrCreate()
+    )
+
+spark = get_spark()
+spark.conf.set("spark.sql.parquet.enableVectorizedReader", "false")
+
+def subtractDays(
+    numberOfDays: int
+) -> str:
+    '''
+    Subtract n days from todays date.
+
+    Args:
+        numberOfDays (int): Number of days to subtract.
+
+    Returns:
+        str: Date n days before.
+    '''    
+    
+    yesterday = datetime.now() - timedelta(numberOfDays)
+    return yesterday.strftime('%Y-%m-%d')
+
+def load_spark_parquet(base_path, date):
+    start_date = datetime.strptime(date, "%Y-%m-%d")
+    # start_date = date
+
+    valid_paths = []
+    for i in range(90):
+        path = f"{base_path}/day={(start_date - timedelta(days=i)).strftime('%Y-%m-%d')}/"
+        try:
+            spark.read.parquet(path).limit(1)  
+            valid_paths.append(path)
+        except AnalysisException:
+            pass  
+
+    df = spark.read.parquet(*valid_paths)
+    return df
+
+d_date = subtractDays(2)
+
+base_path = "gs://wynk-ml-workspace/projects/rails_reranking/user_watch_history_new/v1"
+watch_df = load_spark_parquet(base_path, d_date).cache()  
+
+df_result = watch_df.groupBy("userId") \
+              .agg(F.array_distinct(F.flatten(F.collect_list("IDs"))).alias("deduped_IDs"))
+
+movie_df = spark.read.parquet(f'gs://wynk-ml-workspace/projects/xstream_nlu/catalog-db/{d_date}/enriched_movie.parquet')
+tv_df = spark.read.parquet(f'gs://wynk-ml-workspace/projects/xstream_nlu/catalog-db/{d_date}/enriched_tv.parquet')
+
+def extract_l2(df):
+    return (
+        df
+        .select("ID", "Title", F.explode(F.col("GptMeta.L2Tags")).alias("l2"), 
+                F.array_union(
+                F.slice(F.col("Actors"), 1, 5), 
+                F.slice(F.col("Directors"), 1, 5)
+        ).alias("People"))
+        .filter(F.col("l2.Relevance").isin("Extremely Relevant", "Highly Relevant", "Relevant"))
+        .select(
+            F.col("ID"),
+            F.col("Title"),
+            F.col("l2.Tag").alias("L2Tag"),
+            F.explode("People").alias("People")
+        )
+    )
+
+content_df = extract_l2(movie_df).unionByName(extract_l2(tv_df))
+content_df.cache()
+
+mongo_host = "10.161.216.23"
+mongo_port = 27017
+mongo_user = "admin"
+mongo_password = "IAJNSR$:123xyzesqipy"
+mongo_db = "metadata_db"
+
+def get_mongo_string(mongo_host, mongo_port, mongo_user, mongo_password, mongo_db):
+    return f"mongodb://{mongo_user}:{quote_plus(mongo_password)}@{mongo_host}:{mongo_port}/{mongo_db}?authSource=admin"
+   
+mongo_string = get_mongo_string(mongo_host, mongo_port, mongo_user, mongo_password, mongo_db)
+
+client = MongoClient(mongo_string)  
+db = client[mongo_db] 
+
+topics_coll = db['topics']
+
+cursor = topics_coll.find(
+    {"published_count": {"$gt": 10}},  
+    {"_id": 0, "name": 1}  
+)
+
+# l2_coll = db['l2_tags_v2']
+
+# cursor = l2_coll.find(
+#     {
+#         "publish_count_contents": {"$gt": 10},
+#         "is_hero_tag": True  # Filters for hero tags only
+#     }, 
+#     {"_id": 0, "name": 1}
+# )
+
+topics_tag_names = [doc["name"] for doc in cursor]
+
+# l2_tag_names = [doc["name"] for doc in l2_coll.find({"publish_count_contents": {"$gt": 100}}, {"_id":0,"name":1})]
+# l2_tag_names = [doc["name"] for doc in cursor]
+
+# content_df = content_df.filter(F.col("L2Tag").isin(l2_tag_names))
+
+filtered_content_df = content_df.filter(F.col("L2Tag").isin(topics_tag_names))
+filtered_content_df = filtered_content_df.filter(F.col("People").isin(topics_tag_names))
+# filtered_content_df.show(truncate=False)
+
+meta_df = filtered_content_df.groupBy("ID").agg(
+    F.first("Title").alias("Title"),
+    F.collect_set("L2Tag").alias("L2Tags"),
+    F.collect_set("People").alias("People")  
+)
+
+df_user_content = df_result.select(
+    "userId",
+    F.explode("deduped_IDs").alias("content_id")
+).dropDuplicates()
+
+# Add interaction
+df_user_content = df_user_content.withColumn("interaction", F.lit(1))
+
+# Filter users with >=3 interactions
+user_activity = df_user_content.groupBy("userId").count()
+active_users = user_activity.filter("count >= 3").select("userId")
+df_user_content_active = df_user_content.join(active_users, "userId")
+
+# Hash IDs to numeric indices
+df_indexed = df_user_content_active.withColumn(
+    "user_index", F.hash(F.col("userId")).cast("long")
+).withColumn(
+    "content_index", F.hash(F.col("content_id")).cast("long")
+).withColumn(
+    "interaction", F.lit(1)
+)
+
+df_indexed = df_indexed.repartition(2000)
+df_indexed.cache()
+
+df_indexed = df_indexed \
+    .withColumn("user_index", F.col("user_index").cast("int")) \
+    .withColumn("tag_index", F.col("content_index").cast("int")) \
+    .withColumn("watch_count", F.col("interaction").cast("float"))
+
+als = ALS(
+    userCol="user_index",
+    itemCol="content_index",
+    ratingCol="interaction",
+    implicitPrefs=True,
+
+    # Core model tuning
+    rank=40,                 
+    maxIter=10,              
+    regParam=0.08,           
+    alpha=20,      
+
+    # Stability & performance
+    coldStartStrategy="drop",
+    nonnegative=True,
+
+    # Distributed performance tuning
+    numUserBlocks=200,       
+    numItemBlocks=50,        
+    checkpointInterval=2    
+)
+
+model = als.fit(df_indexed)
+
+user_recs = model.recommendForAllUsers(100)
+
+user_recs = user_recs.withColumn("rec", F.explode("recommendations"))
+user_recs = user_recs.select(
+    F.col("user_index"),
+    F.col("rec.content_index"),
+    F.col("rec.rating")
+)
+
+# Create mapping from content_id to content_index
+content_mapping = df_indexed.select("content_index", "content_id").dropDuplicates()
+
+user_recs = user_recs.join(content_mapping, "content_index", "left")
+
+recs_with_meta = user_recs.join(meta_df, user_recs.content_id == meta_df.ID, "left")
+recs_with_meta = recs_with_meta.drop("ID")
+
+
+def get_top_tags(df, tag_col, top_n):
+    df_exploded = df.withColumn("tag", F.explode(F.col(tag_col)))
+    
+    df_ranked = df_exploded.groupBy("user_index", "tag") \
+        .agg(F.sum("rating").alias("tag_score"))
+    
+    window = Window.partitionBy("user_index").orderBy(F.desc("tag_score"))
+
+    window_score = Window.partitionBy("user_index", "tag_score").orderBy(F.desc("tag_score"))
+    df_unique = df_ranked.withColumn("row_num", F.row_number().over(window_score)) \
+                         .filter(F.col("row_num") == 1) \
+                         .drop("row_num")
+    
+    return df_unique.withColumn("rank", F.row_number().over(window)) \
+                    .filter(f"rank <= {top_n}")
+
+df_top_l2 = get_top_tags(recs_with_meta, "L2Tags", 8)
+df_top_people = get_top_tags(recs_with_meta, "People", 2)
+
+df_top_tags = df_top_l2.unionByName(df_top_people)
+
+# Create mapping from user_index to userId
+user_mapping = df_indexed.select("user_index", "userId").dropDuplicates()
+df_top_tags = df_top_tags.join(user_mapping, "user_index", "left")
+
+final_user_tags = df_top_tags.select(
+    "userId", "tag", "tag_score"
+).orderBy("userId", "tag_score", ascending=False)
+
+# Rank tags per user by tag_score
+window = Window.partitionBy("userId").orderBy(F.col("tag_score").desc())
+
+df_ranked = df_top_tags.withColumn("rank", F.row_number().over(window))
+
+# Keep only top 10 tags per user
+df_top10 = df_ranked.filter(F.col("rank") <= 10)
+
+# Aggregate into list
+final_df = df_top10.groupBy("userId").agg(
+    F.collect_list("tag").alias("tags")
+)
+
+output_path = f"gs://wynk-ml-workspace/ritika/als_uid_topic_output/{d_date}/"
+
+(
+    final_df
+    .repartition(200)  
+    .write
+    .mode("overwrite")
+    .parquet(output_path)
+)
---
+++ tf_idf_topic_reco.py
@@ -0,0 +1,299 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.utils import AnalysisException
+from pyspark.sql import functions as F
+from pyspark.sql.window import Window
+from datetime import date, datetime, timedelta
+
+from pymongo import MongoClient
+from urllib.parse import quote_plus
+import numpy as np
+import gcsfs
+
+def get_spark():
+    return (
+        SparkSession.builder
+        .appName("TF-IDF Topic Reco")
+        .getOrCreate()
+    )
+
+spark = get_spark()
+spark.conf.set("spark.sql.parquet.enableVectorizedReader", "false")
+
+def subtractDays(
+    numberOfDays: int
+) -> str:
+    '''
+    Subtract n days from todays date.
+
+    Args:
+        numberOfDays (int): Number of days to subtract.
+
+    Returns:
+        str: Date n days before.
+    '''    
+    
+    yesterday = datetime.now() - timedelta(numberOfDays)
+    return yesterday.strftime('%Y-%m-%d')
+
+def load_spark_parquet(base_path, date):
+    start_date = datetime.strptime(date, "%Y-%m-%d")
+
+    valid_paths = []
+    for i in range(90):
+        path = f"{base_path}/day={(start_date - timedelta(days=i)).strftime('%Y-%m-%d')}/"
+        try:
+            spark.read.parquet(path).limit(1)  
+            valid_paths.append(path)
+        except AnalysisException:
+            pass  
+
+    df = spark.read.parquet(*valid_paths)
+    return df
+
+
+def load_spark_parquet_recent(base_path, date):
+    start_date = datetime.strptime(date, "%Y-%m-%d")
+
+    valid_paths = []
+    for i in range(15):
+        path = f"{base_path}/day={(start_date - timedelta(days=i)).strftime('%Y-%m-%d')}/"
+        try:
+            spark.read.parquet(path).limit(1)  
+            valid_paths.append(path)
+        except AnalysisException:
+            pass  
+
+    df = spark.read.parquet(*valid_paths)
+    return df
+    
+d_date = subtractDays(2)
+
+base_path = "gs://wynk-ml-workspace/projects/rails_reranking/user_watch_history_new/v1"
+watch_df = load_spark_parquet(base_path, d_date).cache()  
+
+watch_df_recent = load_spark_parquet_recent(base_path, d_date).cache()  
+
+movie_df = spark.read.parquet(f'gs://wynk-ml-workspace/projects/xstream_nlu/catalog-db/{d_date}/enriched_movie.parquet')
+tv_df = spark.read.parquet(f'gs://wynk-ml-workspace/projects/xstream_nlu/catalog-db/{d_date}/enriched_tv.parquet')
+
+def extract_l2(df):
+    return (
+        df
+        .select("ID", "Title", F.explode(F.col("GptMeta.L2Tags")).alias("l2"),
+                F.array_union(
+            F.slice(F.col("Actors"), 1, 5),   
+            F.slice(F.col("Directors"), 1, 5)             
+        ).alias("People"))
+        .filter(F.col("l2.Relevance").isin("Extremely Relevant", "Highly Relevant", "Relevant"))
+        .select(
+            F.col("ID"),
+            F.col("Title"),
+            F.col("l2.Tag").alias("L2Tag"),
+            F.explode("People").alias("People")
+        )
+    )
+
+content_df = extract_l2(movie_df).unionByName(extract_l2(tv_df))
+content_df.cache()
+
+mongo_host = "10.161.216.23"
+mongo_port = 27017
+mongo_user = "admin"
+mongo_password = "IAJNSR$:123xyzesqipy"
+mongo_db = "metadata_db"
+
+def get_mongo_string(mongo_host, mongo_port, mongo_user, mongo_password, mongo_db):
+    return f"mongodb://{mongo_user}:{quote_plus(mongo_password)}@{mongo_host}:{mongo_port}/{mongo_db}?authSource=admin"
+   
+mongo_string = get_mongo_string(mongo_host, mongo_port, mongo_user, mongo_password, mongo_db)
+
+client = MongoClient(mongo_string)  
+db = client[mongo_db] 
+
+# l2_coll = db['l2_tags_v2']
+
+# cursor = l2_coll.find(
+#     {
+#         "publish_count_contents": {"$gt": 10},
+#         "is_hero_tag": True  # Filters for hero tags only
+#     }, 
+#     {"_id": 0, "name": 1}
+# )
+
+# l2_tag_names = [doc["name"] for doc in cursor]
+
+topics_coll = db['topics']
+
+cursor = topics_coll.find(
+    {"published_count": {"$gt": 10}},  
+    {"_id": 0, "name": 1}  
+)
+
+topics_tag_names = [doc["name"] for doc in cursor]
+
+# filtered_content_df = content_df.filter(F.col("L2Tag").isin(l2_tag_names))
+filtered_content_df = content_df.filter(F.col("L2Tag").isin(topics_tag_names))
+filtered_content_df = filtered_content_df.filter(F.col("People").isin(topics_tag_names))
+# filtered_content_df.show(truncate=False)
+
+fs = gcsfs.GCSFileSystem()
+file_path = f"gs://wynk-ml-workspace/projects/rails_reranking/idf_vectors_new/v1/day={d_date}/idf_vector.npy"
+
+with fs.open(file_path, 'rb') as f:
+    idf_vector = np.load(f)
+
+# Broadcast IDF
+sc = spark.sparkContext
+idf_broadcast = sc.broadcast(idf_vector)
+
+# Create mapping L2Tag to index
+l2_tag_to_index = {tag: idx for idx, tag in enumerate(topics_tag_names)}
+l2_tag_index_broadcast = sc.broadcast(l2_tag_to_index)
+
+UID_COLUMN = "userId"
+ITEM_COLUMN = "IDs"
+
+# FULL DATA
+user_items_full_df = (
+    watch_df
+    .withColumn("ID", F.explode(F.col(ITEM_COLUMN)))
+    .select(F.col(UID_COLUMN).alias("uid"), "ID")
+    .distinct()
+)
+
+# LAST 15 DAYS
+user_items_recent_df = (
+    watch_df_recent
+    .withColumn("ID", F.explode(F.col(ITEM_COLUMN)))
+    .select(F.col(UID_COLUMN).alias("uid"), "ID")
+    .distinct()
+)
+
+# Join with content
+user_content_full_df = user_items_full_df.join(filtered_content_df, on="ID", how="inner")
+
+user_content_recent_df = user_items_recent_df.join(filtered_content_df, on="ID", how="inner")
+
+tf_full_df = (
+    user_content_full_df
+    .groupBy("uid", "L2Tag")
+    .agg(F.count("*").alias("tf"))
+)
+
+tf_recent_df = (
+    user_content_recent_df
+    .groupBy("uid", "L2Tag")
+    .agg(F.count("*").alias("tf"))
+)
+
+def compute_tfidf(l2tag, tf):
+    try:
+        idx = l2_tag_index_broadcast.value[l2tag]
+        return float(tf * idf_broadcast.value[idx])
+    except Exception:
+        return 0.0
+
+tfidf_udf = F.udf(compute_tfidf, "double")
+
+tfidf_full_df = tf_full_df.withColumn("tfidf", tfidf_udf(F.col("L2Tag"), F.col("tf")))
+tfidf_recent_df = tf_recent_df.withColumn("tfidf", tfidf_udf(F.col("L2Tag"), F.col("tf")))
+
+window_spec = Window.partitionBy("uid").orderBy(F.desc("tfidf"))
+
+rank_full_df = tfidf_full_df.withColumn("rank", F.row_number().over(window_spec))
+rank_recent_df = tfidf_recent_df.withColumn("rank", F.row_number().over(window_spec))
+
+recent_top3 = (
+    rank_recent_df
+    .filter(F.col("rank") <= 3)
+    .groupBy("uid")
+    .agg(F.collect_list("L2Tag").alias("recent_tags"))
+)
+full_topN = (
+    rank_full_df
+    .filter(F.col("rank") <= 15)
+    .groupBy("uid")
+    .agg(F.collect_list("L2Tag").alias("full_tags"))
+)
+
+tf_people_df = (
+    user_content_full_df
+    .groupBy("uid", "People")
+    .agg(F.count("*").alias("tf"))
+)
+
+# Rank People
+window_people = Window.partitionBy("uid").orderBy(F.desc("tf"))
+
+rank_people_df = tf_people_df.withColumn("rank", F.row_number().over(window_people))
+
+top2_people = (
+    rank_people_df
+    .filter(F.col("rank") <= 2)
+    .groupBy("uid")
+    .agg(F.collect_list("People").alias("people_tags"))
+)
+
+combined_tags = (
+    full_topN
+    .join(recent_top3, on="uid", how="left")
+    .join(top2_people, on="uid", how="left")
+    
+    # Remove overlap from full tags
+    .withColumn(
+        "filtered_full_tags",
+        F.expr("array_except(full_tags, recent_tags)")
+    )
+    
+    # Take top 5 from remaining
+    .withColumn(
+        "top5_full",
+        F.expr("slice(filtered_full_tags, 1, 5)")
+    )
+    
+    # Final 10 tags
+    .withColumn(
+        "final_tags",
+        F.expr("concat(recent_tags, top5_full, people_tags)")
+    )
+    
+    .select("uid", "final_tags")
+)
+
+# combined_tags = (
+#     full_top5.join(recent_top2, on="uid", how="left")
+#     .withColumn(
+#         "remaining_tags",
+#         F.expr("slice(array_except(full_tags, recent_tags),1,3)")
+#     )
+#     .withColumn(
+#         "top5_topic",
+#         F.expr("concat(recent_tags, remaining_tags)")
+#     )
+#     .select("uid", "top5_topic")
+# )
+
+user_meta_df = (
+    user_content_full_df
+    .groupBy("uid")
+    .agg(
+        F.collect_set("ID").alias("nf_ids"),
+        F.collect_set("Title").alias("titles")
+    )
+)
+
+final_df = user_meta_df.join(combined_tags, on="uid", how="left")
+
+final_df = final_df.filter(F.size(F.col("nf_ids")) >= 2)
+
+output_path = f"gs://wynk-ml-workspace/ritika/uid_topic_output/{d_date}/"
+
+(
+    final_df
+    .repartition(200) 
+    .write
+    .mode("overwrite")
+    .parquet(output_path)
+)
+
+print(f" Parquet written to {output_path}")
